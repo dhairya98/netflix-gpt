fe1e7887dda42d11332a1e3388ba0d65ffcf452d
Dhairya Anchal <dharya.anchal@gmail.com>
2025-04-26 23:27:03 +0530

    code improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, USER_ICON } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -31,7 +32,7 @@ const Header = () => {
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-full z-10 flex justify-between">
       <img
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="Netflix logo"
         aria-hidden="true"
         className="w-44"
@@ -39,7 +40,7 @@ const Header = () => {
       {user && (
         <div className="flex items-center">
           <img
-            src="https://i.pinimg.com/564x/5b/50/e7/5b50e75d07c726d36f397f6359098f58.jpg"
+            src={USER_ICON}
             alt="User Icon"
             aria-hidden="true"
             className="w-12 h-12 p-2"
