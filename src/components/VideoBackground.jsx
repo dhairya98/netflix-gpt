b384df4485b98e175a61575a5394168cef278cfb
Dhairya Anchal <dharya.anchal@gmail.com>
2025-04-29 08:39:43 +0530

    cosmetic fix

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -3,16 +3,16 @@ import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-    useMovieTrailer(movieId);
-    const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+  useMovieTrailer(movieId);
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
   return (
     <div className="w-screen">
       <iframe
-      className="w-screen aspect-video h-[700px]"
+        className="w-screen aspect-video h-[700px]"
         width="560"
         height="315"
         src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1`}
-        title="YouTube video player"
+        title="Video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
